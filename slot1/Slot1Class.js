763d8274113f11a8fdb30f0c5d3727df6498a0c7
Nhut Duy <ngophamnhutduy050302@gmail.com>
2025-10-01 13:09:33 +0700

    mobile app slot 1

---
+++ slot1/Slot1Class.js
@@ -0,0 +1,15 @@
+//import thu vien
+import React from "react";
+import { Text, View } from "react-native";
+//dinh nghia class
+class Slot1Class extends React.Component{
+render(){
+    return(
+        <View style={{backgroundColor:'blue'}}>
+            <Text>Đây là trò chơi của Doffy</Text>
+        </View>
+    )
+}
+}
+// export component
+export default Slot1Class;
---
+++ slot1/Slot1Funtion.js
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+//2 dinh nghia ham
+const Slot1Funtion = () =>{
+return(
+    <View>
+        <Text>đay là function</Text>
+    </View>
+)
+}
+//3 export 
+export default Slot1Funtion;
